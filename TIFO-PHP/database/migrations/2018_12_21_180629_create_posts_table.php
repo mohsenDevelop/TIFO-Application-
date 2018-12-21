260602ab77ca71eaea3fae8a9e7daccd512165a2
Mohammad <mohammad-rajaei-jm@users.noreply.github.com>
2018-12-21 23:06:33 +0330

    DB Model

---
+++ TIFO-PHP/database/migrations/2018_12_21_180629_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("title",30);
+            $table->text("content");
+            $table->string("summary",200);
+            $table->integer("author_id",FALSE,TRUE)->unique();
+            $table->integer("author_type")->unsigned();
+            $table->string("picture",60);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ TIFO-PHP/database/migrations/2018_12_21_180716_create_students_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string("name",30);
+            $table->string("phone",11)->unique();
+            $table->string("token",8);
+            $table->string("melli_number",10)->unique()->nullable();
+            $table->tinyInteger("confirmed")->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
